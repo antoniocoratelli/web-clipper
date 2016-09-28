b039162b4ee678d9dd936e0c7ae05e4af0e6a63c
Antonio Coratelli <antoniocoratelli@gmail.com>
2016-09-28 09:32:10 +0200

    Remove unneeded function

--- web-clipper.py
+++ web-clipper.py
@@ -82,14 +82,6 @@ class WebClipper:
         self.filename = filename
 
     def ask_filename(self):
-        def center(toplevel):
-            toplevel.update_idletasks()
-            w = toplevel.winfo_screenwidth()
-            h = toplevel.winfo_screenheight()
-            size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-            x = w/2 - size[0]/2
-            y = h/2 - size[1]/2
-            toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
         f = DialogAskString(
             title="web-clipper",
             text="Write filename without extension.",
